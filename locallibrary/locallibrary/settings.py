86b181eeec04c3b0ab9c857182d6f8a89ccb4f02
YuriyB <paganism@mail.ru>
2018-12-06 23:43:02 +0300

    modified login via VK

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -137,5 +137,18 @@ SOCIAL_AUTH_VK_OAUTH2_KEY = '6774700'
 SOCIAL_AUTH_VK_OAUTH2_SECRET = '4qGrPwWPiKBZHlKWhQHu'
 
 SOCIAL_AUTH_PIPELINE = (
+        #'social_core.pipeline.social_auth.associate_by_email',
+        'social_core.pipeline.social_auth.social_details',
+        'social_core.pipeline.social_auth.social_uid',
+        'social_core.pipeline.social_auth.social_user',
+        'social_core.pipeline.user.get_username',
+        'social_core.pipeline.user.create_user',
+        'social_core.pipeline.social_auth.associate_user',
+        'social_core.pipeline.social_auth.load_extra_data',
+        'social_core.pipeline.user.user_details',
         'social_core.pipeline.social_auth.associate_by_email',
 )
+# SOCIAL_AUTH_VK_OAUTH2_SCOPE = ['email']
+SOCIAL_AUTH_RAISE_EXCEPTIONS = True
+SOCIAL_AUTH_RAISE_EXCEPTIONS = True
+RAISE_EXCEPTIONS = True
